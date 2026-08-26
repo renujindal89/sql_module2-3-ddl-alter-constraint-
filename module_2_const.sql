e030a7edd888beacb04b64e813c0e57cc6f71ccd
renujindal89 <renu.jindal@complereinfosystem.com>
2026-08-26 12:17:16 +0530

    Add files via upload

--- module_2_const.sql
+++ module_2_const.sql
@@ -105,6 +105,10 @@ use empc1;
 drop database empc;
 
 select * from tuesday_work;
+
+-- copy data from one table to another table
+-- ⚠️ Note: This copies the data and column structure,
+--  but usually does not copy constraints such as PRIMARY KEY, FOREIGN KEY, UNIQUE, CHECK, etc
 CREATE TABLE new_table  AS
 SELECT *
 FROM tuesday_work;
@@ -116,24 +120,19 @@ select * from tuesday_work;
 
 
 -- Copy data into an existing table  -- col must match
-INSERT INTO new_table_name
+INSERT INTO new_table
 SELECT *
-FROM old_table_name;
+FROM tuesday_work;
 
 
--- second option
+-- second option copy only selected column
 
-INSERT INTO new_table_name (emp_id, name, email)  
+INSERT INTO new_table (emp_id, name, email)  
 SELECT emp_id, name, email
 FROM old_table_name;
 
 -- third option 
 
-CREATE TABLE company_database.employee AS
-SELECT *
-FROM company_db.employee;
--- ⚠️ Note: This copies the data and column structure,
---  but usually does not copy constraints such as PRIMARY KEY, FOREIGN KEY, UNIQUE, CHECK, etc
 
 -- or complete table structure with constraints + data
 CREATE TABLE company_database.employee 
@@ -142,4 +141,121 @@ LIKE company_db.employee;
 INSERT INTO company_database.employee
 SELECT *
 FROM company_db.employee;
+
+
+-- 5. Change CHECK Constraint
+
+desc employee_const;
+SHOW CREATE TABLE employee_const;
+
+ALTER TABLE employee_const
+DROP CHECK chk_age;
+
+ALTER TABLE employee_const
+ADD CONSTRAINT chk_age2
+CHECK (age between 18 and 40);
+
+
+CREATE TABLE Employees1 (
+    emp_id INT ,
+    email VARCHAR(100),
+    name VARCHAR(50),
+    age INT,
+    salary DECIMAL(10,2) DEFAULT 25000,
+
+    CONSTRAINT pk_emp PRIMARY KEY (emp_id),
+
+    CONSTRAINT uq_email UNIQUE (email),
+
+    CONSTRAINT chk_age1 CHECK (age >= 18),
+
+    CONSTRAINT chk_salary CHECK (salary >= 10000)
+);
+
+show create table Employees1;
+
+create table parent_course(course_name varchar(10) primary key);
+insert into parent_course values ('sql'),('python'),('bi');
+select * from parent_course;
+create table child_stu
+(stuname varchar(20),course_name varchar(10) ,
+foreign key (course_name) 
+references parent_course(course_name));
+
+desc child_stu;
+
+desc employee_const;
+
+insert into child_stu values ('vanshika','sql');
+select * from child_stu;
+select * from parent_course;
+insert into child_stu values ('yuvraj','ai');
+insert into child_stu values ('renu','java');
+insert into child_stu values ('neha','bi');
+insert into parent_course values ('java');
+
+-- delete
+delete from parent_course
+where course_name='sql';
+
+-- python delete
+delete from parent_course
+where course_name='python';
+
+select * from parent_course;
+
+-- updation in parent table
+update parent_course
+set course_name = 'bisiness'
+where course_name='bi';
+
+drop table child_stu;
+
+create table child_stu (stuname varchar(10),course_name varchar(20),
+foreign key (course_name) references parent_course(course_name)
+on delete cascade
+on update cascade);
+insert into child_stu values ('yuvraj','bi');
+insert into child_stu values ('renu','java');
+insert into child_stu values ('neha','bi');
+insert into child_stu values('vanshika','sql');
+
+
+
+
+select * from parent_course;
+select * from child_stu;
+
+
+delete from parent_course
+where course_name='sql';
+
+update parent_course
+set course_name = 'bisiness'
+where course_name='bi';
+
+drop table child_stu;
+
+create table child_stu (stuname varchar(10),course_name varchar(20),
+foreign key (course_name) references parent_course(course_name)
+on delete set null
+on update cascade);
+
+
+
+insert into child_stu values ('yuvraj','bisiness');
+insert into child_stu values ('renu','java');
+insert into child_stu values ('neha','bisiness');
+
+select * from child_stu;
+select * from parent_course;
+
+delete from parent_course
+where course_name='java';
+
+-- finished
+
+
+
+
 
